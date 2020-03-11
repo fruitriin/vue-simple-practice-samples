cddc1db6d3a07375f931532deff6de74d847f15e
Riin Fruit <nassii74@gmail.com>
2020-03-11 22:15:29 +0900

    fix: シンプルにするためにHello Worldコンポーネントを一旦削除

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
+    <h1>{{ message }}</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data(){
+    return {
+      message: "Hello Vue!"
+    }
   }
 };
 </script>
