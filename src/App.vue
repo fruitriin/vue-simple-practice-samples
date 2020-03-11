31a1211d600ad61f5592d15c7f4532d5f584511c
Riin Fruit <nassii74@gmail.com>
2020-03-11 23:12:35 +0900

    Sample3: メソッドとイベントコール

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@
     <p>このinputを編集すると見出し(message)がリアルタイムに変わる</p>
     <input v-model="message">
     <p>このinputを編集するとカウントが(count)変わる</p>
-    <input type="number" v-model="count">
+    <input type="number" v-model="count"><br>
+    <!-- v-on:イベント でイベントを起こせる -->
+    <button v-on:click="alert">アラートがでるボタン</button>
+    <!-- v-on:イベント と @イベント は同じ（省略記法）-->
+    <button @click="alertMessage">(message)がアラートするボタン</button>
   </div>
 </template>
 
@@ -17,6 +21,16 @@ export default {
       message: "Hello Vue!",
       count: 1
     };
+  },
+  methods: {
+    alert(){
+      // 呼び出されるとアラートがでます
+      window.alert("ぴこーん！")
+    },
+    alertMessage(){
+      // スクリプト内でdataを参照するには this. のあとに変数名
+      window.alert(this.message)
+    }
   }
 };
 </script>
